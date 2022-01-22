4877afb04adcf271bec33bb78dfb157916200acd
abdulaziz quranfoleh <abdulaziz.quranfoleh@gmail.com>
2022-01-22 23:00:07 +0200

    main

--- admin/index.php
+++ admin/index.php
@@ -129,7 +129,7 @@
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="tables.html">
+                <a class="nav-link" href="tables.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Tables</span></a>
             </li>
@@ -382,8 +382,16 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                                (user):</div>
+                                                <?php
+                                                $connection= mysqli_connect("localhost","root","","form_db");
+                                                $query = "SELECT id FROM users ORDER BY id";
+                                                     $query_run=mysqli_query($connection, $query);
+                                                     $row= mysqli_num_rows($query_run);
+                                                        echo '<h1> '.$row.'</h1>';
+                                                            ?>
+
+                                       
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -401,6 +409,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 Earnings (Annual)</div>
+                                                
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
                                         </div>
                                         <div class="col-auto">
--- admin/tables.php
+++ admin/tables.php
@@ -135,7 +135,7 @@
 
             <!-- Nav Item - Tables -->
             <li class="nav-item active">
-                <a class="nav-link" href="tables.html">
+                <a class="nav-link" href="tables.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Tables</span></a>
             </li>
